3684521e7a86d41c1e376b75731681115f478c6f
lakshmi-ummidi <86336157+lakshmisairaghava@users.noreply.github.com>
2023-02-07 17:20:12 +0530

    Create PrintSubsets.cpp

---
+++ PrintSubsets.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void PrintSubsets(int ind, int arr[], int n, vector<int>&ds){
+  if(ind == n){
+    for(int it: ds) cout<<it<<" ";
+    cout<<"\n";
+    return;}
+  ds.push_back(
+  
+int main(){
+  int n = 3;
+  int arr[n] = [1, 2, 3];
+  PrintSubsets(0, arr, n, {});}
