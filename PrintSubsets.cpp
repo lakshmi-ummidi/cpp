d2f1d21a8b59a5370d06402ccddd8e2b46a478ec
lakshmi-ummidi <86336157+lakshmi-ummidi@users.noreply.github.com>
2023-02-07 17:58:41 +0530

    PrintSubsets.cpp

--- PrintSubsets.cpp
+++ PrintSubsets.cpp
@@ -1,13 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void PrintSubsets(int ind, int arr[], int n, vector<int>&ds){
+void PrintSubsets(int ind, int arr[], int n, vector<int> &ds){
   if(ind == n){
     for(int it: ds) cout<<it<<" ";
     cout<<"\n";
     return;}
-  ds.push_back();
+  ds.push_back(arr[ind]);
+  PrintSubsets(ind+1, arr, n, ds);
+  ds.pop_back();
+  PrintSubsets(ind+1, arr, n, ds);}
   
 int main(){
   int n = 3;
-  int arr[n] = [1, 2, 3];
-  PrintSubsets(0, arr, n, {});}
+  int arr[n] = {1, 2, 3};
+  vector<int> ds;
+  PrintSubsets(0, arr, n, ds);
+    
+}
